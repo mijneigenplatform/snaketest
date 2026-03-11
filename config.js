77a6b0656e5ae2f7fabce3f013f000bca8f6191a
mijneigenplatform <mijneigenplatform@gmail.com>
2026-03-11 15:30:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SNAKE_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: https://fogxuclgcbojtcamcoqh.supabase.co
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvZ3h1Y2xnY2JvanRjYW1jb3FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMjYyNDcsImV4cCI6MjA4ODgwMjI0N30.ki-ul_DKvRV86K_04cGXAWP6LGVCb48wiBooxukUaFY",
   leaderboardTable: "snake_scores"
 };
